eca8b07a6fd5e523a9c50e3c0e19e55a631a85cd
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-03-13 16:56:22 +0100

    upload

--- database_api/__init__.py
+++ database_api/__init__.py
@@ -1,22 +1,56 @@
 import sys
 import enum
 import traceback
+import os
+
 from contextlib import contextmanager
 from datetime import datetime, date, time
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine, Column, Integer, DateTime, func
+from apscheduler.schedulers.background import BackgroundScheduler
+from werkzeug.utils import secure_filename
 
 from .porting import data_export_, data_import_
 from .alembic_migration_check import alembic_migration_check
+from api.storage import upload_file_to_s3
 
 
 engine = None
 Base = declarative_base()
 
+def db_backup(interval, engine):
+  timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  print(f"[{timestamp}] Backup eseguito! (Intervallo: {interval})")
+  try:
+    zip_filename = data_export_(engine)
+    
+    with open(zip_filename, "rb") as file:
+      upload_file_to_s3(file, 'backuptestcl', secure_filename(zip_filename))
+      
+    os.remove(zip_filename)
+  except Exception as e:
+    print(f"Errore durante il backup: {e}")
 
-def set_database(url):
+
+def schedule_backup(engine):
+  scheduler = BackgroundScheduler()
+  
+  scheduler.add_job(lambda: db_backup("Giornaliero", engine), "interval", minutes=1)    # Ogni giorno
+
+  # scheduler.add_job(lambda: db_backup("Giornaliero", engine), "interval", days=1)    # Ogni giorno
+  # scheduler.add_job(lambda: db_backup("Settimanale", engine), "interval", weeks=1)   # Ogni settimana
+  # scheduler.add_job(lambda: db_backup("Mensile", engine), "cron", day=1, hour=0, minute=0)  # Ogni 1° del mese a mezzanotte
+  
+  scheduler.start()
+  
+
+def set_database(url, cmd = False):
   global engine
   engine = create_engine(url, pool_pre_ping=True)
+  
+  if os.environ.get('IS_DEV', 1) != 1 and not cmd:
+    schedule_backup(engine)
+    
   alembic_migration_check(engine, Session)
   return engine
 
@@ -70,8 +104,8 @@ class BaseEnum(enum.Enum):
 
 
 def data_export():
-  data_export_(set_database(sys.argv[1]))
+  data_export_(set_database(sys.argv[1], True))
 
 
 def data_import():
-  data_import_(set_database(sys.argv[1]), sys.argv[2])
+  data_import_(set_database(sys.argv[1], True), sys.argv[2])
