54d94bb001cee41a81125747029a660861206b43
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-03-14 15:51:55 +0100

    test auto delete

--- database_api/backup.py
+++ database_api/backup.py
@@ -1,6 +1,6 @@
 import os
 
-from api.storage import upload_file_to_s3
+from api.storage import upload_file_to_s3, delete_file_from_s3, list_files_in_s3
 from .porting import data_export_
 from werkzeug.utils import secure_filename
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -13,29 +13,41 @@ def get_project_folder():
 
 def schedule_backup(engine):
   scheduler = BackgroundScheduler()
-  
-  scheduler.add_job(lambda: db_backup("Giornaliero", engine), "interval", days=1)    # Ogni giorno
-  scheduler.add_job(lambda: db_backup("Settimanale", engine), "interval", weeks=1)   # Ogni settimana
-  scheduler.add_job(lambda: db_backup("Mensile", engine), "cron", day=1, hour=0, minute=0)  # Ogni 1° del mese a mezzanotte
-  
+  scheduler.add_job(lambda: db_backup(engine), "interval", days=1)    # Ogni giorno
   scheduler.start()
   
   
-def db_backup(interval, engine):
-  timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-  print(f"[{timestamp}] Backup eseguito! (Intervallo: {interval})")
+def db_backup(engine):
+  timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+  print(f"[{timestamp}] Backup eseguito!")
   
   try:
     zip_filename = data_export_(engine)
-    new_zip_filename = f"{interval.lower()}-{zip_filename}"
-    os.rename(zip_filename, new_zip_filename)
     
-    s3_key = f"{get_project_folder()}/{secure_filename(new_zip_filename)}"
+    s3_bucket = 'fastsite-postgres-backup'
+    s3_folder = get_project_folder()
+    s3_key = f"{s3_folder}/{secure_filename(zip_filename)}"
     
-    with open(new_zip_filename, "rb") as file:
-      upload_file_to_s3(file, 'fastsite-postgres-backup', s3_key)
-      
-    os.remove(new_zip_filename)
+    with open(zip_filename, "rb") as file:
+      upload_file_to_s3(file, s3_bucket, s3_key)
+  
+    os.remove(zip_filename)
+    
+    manage_s3_backups(s3_bucket, s3_folder)
     
   except Exception as e:
     print(f"Errore durante il backup: {e}")
+
+def manage_s3_backups(bucket, folder):
+  try:
+    backups = list_files_in_s3(bucket, folder)  
+    backups.sort()
+    
+    if len(backups) > 14:
+      files_to_delete = backups[:len(backups) - 14]  # Prende i più vecchi in eccesso
+      for file_key in files_to_delete:
+        delete_file_from_s3(bucket, file_key)
+        print(f"Eliminato vecchio backup: {file_key}")
+
+  except Exception as e:
+    print(f"Errore nella gestione dei backup su S3: {e}")
