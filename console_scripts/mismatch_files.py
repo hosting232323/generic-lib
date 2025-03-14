4fda367657b4400d659dc59e941377f0a06a9952
Leonardo Paparell <leonardo.paparella98@gmail.comm>
2025-03-15 00:20:32 +0100

    reinserita eccezzione in caso in cui c'è un errore alla connessione al db o a s3

--- console_scripts/mismatch_files.py
+++ console_scripts/mismatch_files.py
@@ -12,24 +12,31 @@ def get_user_input():
     return db_url, query, bucket_name, aws_access_key_id, aws_secret_access_key
 
 def get_db_files(db_url, query):
-    conn = psycopg2.connect(db_url)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    urls = {row[0] for row in cursor.fetchall()}
-    conn.close()
+    try:
+        conn = psycopg2.connect(db_url)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        urls = {row[0] for row in cursor.fetchall()}
+        conn.close()
 
-    return {os.path.basename(url) for url in urls}
+        return {os.path.basename(url) for url in urls}
+    except Exception as e:
+        print(f"Errore nel recupero dei file dal database: {e}")
+        return set()
 
 def get_s3_files(bucket_name, aws_access_key_id, aws_secret_access_key):
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
+    try:
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
 
-    response = s3_client.list_objects_v2(Bucket=bucket_name)
-    return {os.path.basename(obj['Key']) for obj in response.get('Contents', [])}
-    
+        response = s3_client.list_objects_v2(Bucket=bucket_name)
+        return {os.path.basename(obj['Key']) for obj in response.get('Contents', [])}
+    except Exception as e:
+        print(f"Errore nel recupero dei file da S3: {e}")
+        return set()
 
 def run_comparison():
     db_url, query, bucket_name, aws_access_key_id, aws_secret_access_key = get_user_input()
