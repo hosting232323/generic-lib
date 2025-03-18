315b924eaa97ad47215c3390506c8d689847d3c8
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-03-18 18:34:50 +0100

    push test

--- console_scripts/__init__.py
+++ console_scripts/__init__.py
@@ -1,6 +1,8 @@
 from.mismatch_files import run_comparison
 from database_api import data_export, data_import
 from .project_setup import main as setup_project_main
+from .mailer import spam_mail_
+import sys
 
 
 def check_aws_mismatch():
@@ -17,3 +19,6 @@ def db_import():
 
 def setup_project():
   setup_project_main()
+
+def spam_mail():
+  spam_mail_(sys.argv[1], sys.argv[2])
