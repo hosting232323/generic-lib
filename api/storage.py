10374914bc8ea91679e88c97ceb1773fa0d7c149
Leonardo Paparell <leonardo.paparella98@gmail.comm>
2025-03-19 16:33:09 +0100

    add support for environment-specific S3 key handling and improve user input for folder selection

--- api/storage.py
+++ api/storage.py
@@ -4,7 +4,6 @@ import botocore
 from flask import abort
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 s3 = None
@@ -17,9 +16,7 @@ if 'AWS_ACCESS_KEY_ID' in os.environ and 'AWS_SECRET_ACCESS_KEY' in os.environ:
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
   )
 
-
 def storage_decorator(func):
-
   def wrapper(bucket_name: str, key: str, *args, **kwargs):
     if '.' in key:
       extension = key.split('.')[-1]
@@ -30,13 +27,19 @@ def storage_decorator(func):
         'status': 'ko',
         'error': 'File name does not contain an extension'
       }
-
     return func(bucket_name, key, *args, **kwargs)
-
   return wrapper
 
+def get_s3_key(key, is_dev):
+  if is_dev is None:
+    return key 
+  elif is_dev:
+    return f"test/{key}"
+  else:
+    return f"prod/{key}"  
 
-def download_file_from_s3(bucket_name, key):
+def download_file_from_s3(bucket_name, key, is_dev=None):
+  key = get_s3_key(key, is_dev)
   try:
     return s3.get_object(Bucket=bucket_name, Key=str(key))['Body'].read()
   except botocore.exceptions.ClientError as e:
@@ -45,14 +48,14 @@ def download_file_from_s3(bucket_name, key):
     else:
       raise e
 
-
-def delete_file_from_s3(bucket_name, key):
+def delete_file_from_s3(bucket_name, key, is_dev=None):
+  key = get_s3_key(key, is_dev)
   s3.delete_object(Bucket=bucket_name, Key=key)
 
-
-def upload_file_to_s3(file, bucket_name, key):
+def upload_file_to_s3(file, bucket_name, key, is_dev=None):
+  key = get_s3_key(key, is_dev)
   s3.upload_fileobj(file, bucket_name, key)
-  
 
-def list_files_in_s3(bucket, folder = ''):
+def list_files_in_s3(bucket, folder='', is_dev=None):
+  folder = get_s3_key(folder, is_dev) if folder else get_s3_key('', is_dev)
   return [obj['Key'] for obj in s3.list_objects_v2(Bucket=bucket, Prefix=folder)['Contents']]
--- console_scripts/mismatch_files.py
+++ console_scripts/mismatch_files.py
@@ -1,33 +1,59 @@
 import os
 from sqlalchemy import text
-
 from database_api import set_database
 from api.storage import list_files_in_s3
 
-
 def get_user_input():
-  return input('Inserisci l\'URL del database: '), \
-    input('Inserisci la query per ottenere i file: '), \
-    input('Inserisci il nome del bucket S3: ')
+    db_url = input('Inserisci l\'URL del database: ')
+    query = input('Inserisci la query per ottenere i file: ')
+    bucket_name = input('Inserisci il nome del bucket S3: ')
+    folder_input = input('Folder selezionato: (None, prod, test) (default: None): ') or 'None'
+    return db_url, query, bucket_name, folder_input
 
+def parse_is_dev(folder_input):
+    """
+    Converte l'input del folder selezionato in valori booleani o None:
+    - 'None' -> None
+    - 'test' -> True
+    - 'prod' -> False
+    """
+    folder_input = folder_input.lower()
+    if folder_input == "none":
+        return None
+    elif folder_input == "test":
+        return True
+    elif folder_input == "prod":
+        return False
+    else:
+        raise ValueError("Input non valido. Scegli tra 'None', 'prod', o 'test'.")
 
 def get_db_files(db_url, query):
-  with set_database(db_url).connect() as conn:
-    result = conn.execute(text(query))
-    return {os.path.basename(url) for url in {row[0] for row in result}}
+    with set_database(db_url).connect() as conn:
+        result = conn.execute(text(query))
+        return {os.path.basename(url) for url in {row[0] for row in result}}
 
+def get_s3_files(bucket_name, is_dev=None):
+    """
+    Cerca i file su S3 rispettando il prefisso selezionato:
+    - None -> Nessuna sottocartella
+    - True -> Sottocartella 'test/'
+    - False -> Sottocartella 'prod/'
+    """
+    folder_prefix = ''
+    if is_dev is not None:
+        folder_prefix = 'test/' if is_dev else 'prod/'
 
-def get_s3_files(bucket_name):
-  return {os.path.basename(url) for url in list_files_in_s3(bucket_name)}
-
+    return {os.path.basename(url) for url in list_files_in_s3(bucket_name, folder=folder_prefix)}
 
 def run_comparison():
-  db_url, query, bucket_name = get_user_input()
-  db_files = get_db_files(db_url, query)
-  s3_files = get_s3_files(bucket_name)
+    db_url, query, bucket_name, folder_input = get_user_input()
+    is_dev = parse_is_dev(folder_input)
 
-  only_in_s3 = s3_files - db_files
-  only_in_db = db_files - s3_files
+    db_files = get_db_files(db_url, query)
+    s3_files = get_s3_files(bucket_name, is_dev)
 
-  print('File presenti solo in S3:', only_in_s3)
-  print('File presenti solo nel DB:', only_in_db)
+    only_in_s3 = s3_files - db_files
+    only_in_db = db_files - s3_files
+
+    print('File presenti solo in S3:', only_in_s3)
+    print('File presenti solo nel DB:', only_in_db)
