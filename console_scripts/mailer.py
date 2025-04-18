8acaac1a8042aebdb2e8b5ad2780694c12fcff44
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-04-18 14:43:11 +0000

    Fix upload

--- console_scripts/mailer.py
+++ console_scripts/mailer.py
@@ -15,35 +15,34 @@ def spam_mail_main():
         contact_dict = dict(zip(headers, values))  # Crea un dizionario per ogni riga
         contacts_list.append(contact_dict)
   except FileNotFoundError:
-    print(f"Errore: Il file '{contact_path}' non è stato trovato.")
+    print(f'Errore: Il file "{contact_path}" non è stato trovato.')
     return
   except Exception as e:
-    print(f"Errore durante la lettura del file delle email: {e}")
+    print(f'Errore durante la lettura del file delle email: {e}')
     return
 
   try:
     with open(content_path, 'r', encoding='utf-8') as txt_file:
       txt_content = txt_file.read()
   except FileNotFoundError:
-    print(f"Errore: Il file '{content_path}' non è stato trovato.")
+    print(f'Errore: Il file "{content_path}" non è stato trovato.')
     return
   except Exception as e:
-    print(f"Errore durante la lettura del file del contenuto: {e}")
+    print(f'Errore durante la lettura del file del contenuto: {e}')
     return
 
   for i, contact in enumerate(contacts_list, start=1):
     try:
-      email = contact.get("Email", "nessuna_email")  # Recupera l'email dal dizionario
+      email = contact.get('Email', 'nessuna_email')  # Recupera l'email dal dizionario
       personalized_email = txt_content
 
       for key, value in contact.items():
-        placeholder = f"[{key}]"
-        personalized_email = personalized_email.replace(placeholder, value if value else "")
+        personalized_email = personalized_email.replace(f'[{key}]', value if value else '')
 
         send_email(
           receiver_email=email,
           body=personalized_email,
-          subject="Scopri i nostri servizi!"
+          subject='Scopri i nostri servizi!'
         )
         print(f'✅ Email inviata con successo a: {email} ({i}/{len(contacts_list)})')
 
