1f553927e456ef4226c53ca7269c8e9d5df1d9cf
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-10-18 16:21:35 +0200

    fix

--- database_api/backup.py
+++ database_api/backup.py
@@ -1,9 +1,33 @@
 import os
+import sys
+import subprocess
 from datetime import datetime
 from werkzeug.utils import secure_filename
 
-from .porting import data_export
 from api.storage import upload_file_to_s3, delete_file_from_s3, list_files_in_s3
+
+
+def data_export(db_url: str):
+  filename = f'{datetime.now().strftime("%y%m%d%H%M%S")}.dump'
+  subprocess.run([
+    'pg_dump', f'--dbname={db_url}',
+    '--blobs', '--clean', '-Fc',
+    '--verbose', '-f', filename
+  ], check=True)
+  return filename
+
+
+def data_import(db_url: str, filename: str):
+  if not os.path.exists(filename):
+    print(f'File non trovato: {filename}')
+    sys.exit(1)
+
+  subprocess.run([
+    'pg_restore', f'--dbname={db_url}',
+    '--verbose', '--clean',
+    '--if-exists', '--no-privileges',
+    '--no-owner', filename
+  ], check=True)
 
   
 def db_backup(db_url: str, sub_folder: str):
