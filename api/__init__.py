224f3ad803569de21b32aad728dcf23f55932bb4
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-11-01 21:40:15 +0100

    PrefixMiddleware

--- api/__init__.py
+++ api/__init__.py
@@ -27,3 +27,18 @@ def swagger_decorator(func):
 
   wrapper.__name__ = func.__name__
   return wrapper
+
+
+class PrefixMiddleware:
+  def __init__(self, app, prefix=''):
+    self.app = app
+    self.prefix = prefix
+
+  def __call__(self, environ, start_response):
+    if environ['PATH_INFO'].startswith(self.prefix):
+      environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+      environ['SCRIPT_NAME'] = self.prefix
+      return self.app(environ, start_response)
+    else:
+      start_response('404', [('Content-Type', 'text/plain')])
+      return [b'Not Found']
