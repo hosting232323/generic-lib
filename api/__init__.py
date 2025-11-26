bf8978ad6f51365d83aac3cf4eade265a9a37688
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-11-26 19:52:35 +0100

    push

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,21 @@ import os
 import traceback
 from flask import request
 import requests
+from telegram import Bot
 
+IS_DEV = int(os.environ.get('IS_DEV', 1)) == 1
+bot = Bot(os.environ['TELEGRAM_TOKEN'])
+CHAT_ID = -1003410500390
+topic = {
+  "default": 4294967297,
+  "wooffy-be": 4294967352,
+  "italco-be": 4294967355,
+  "chatty-be": 4294967354,
+  "generic-be": 4294967350,
+  "strongbox-be": 4294967353,
+  "generic-booking": 4294967351
+}
+thread_id = topic[os.environ.get("PROJECT_NAME", 'default')]
 
 def error_catching_decorator(func):
   def wrapper(*args, **kwargs):
@@ -11,6 +25,14 @@ def error_catching_decorator(func):
     except Exception:
       error_trace = traceback.format_exc()
       print(error_trace)
+      if not IS_DEV:
+        bot.send_message(
+          chat_id=CHAT_ID,
+          text=error_trace,
+          message_thread_id=thread_id,
+          parse_mode="Markdown"
+        )
+
       requests.post(f'{os.environ["TELEGRAM_ALERT"]}/alert', json={'topic': os.environ["PROJECT_NAME"], 'trace': error_trace})
       return {'status': 'ko', 'message': 'Errore generico'}
 
