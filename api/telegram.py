e8fae7f085845ac37edfb771a054943ef0ff99aa
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-12-02 15:11:03 +0100

    push

--- api/telegram.py
+++ api/telegram.py
@@ -3,9 +3,6 @@ import asyncio
 import threading
 from telegram import Bot
 
-bot = Bot(os.environ['TELEGRAM_TOKEN'])
-IS_DEV = int(os.environ.get('IS_DEV', 1)) == 1
-PROJECT_NAME = os.environ.get('PROJECT_NAME', 'default')
 
 CHAT_ID = -1003410500390
 TELEGRAM_TOPIC = {
@@ -17,7 +14,6 @@ TELEGRAM_TOPIC = {
   'strongbox-be': 4294967353,
   'generic-booking': 4294967351,
 }
-THREAD_ID = TELEGRAM_TOPIC[PROJECT_NAME]
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
@@ -32,10 +28,18 @@ threading.Thread(target=start_loop, args=(loop,), daemon=True).start()
 
 
 def send_telegram_error(trace: str):
+  bot = Bot(os.environ['TELEGRAM_TOKEN'])
+  IS_DEV = int(os.environ.get('IS_DEV', 1)) == 1
+  THREAD_ID = TELEGRAM_TOPIC[os.environ.get('PROJECT_NAME', 'default')]
+  
   if IS_DEV:
     return
 
   async def send_error(text):
-    await bot.send_message(chat_id=CHAT_ID, text=text, message_thread_id=THREAD_ID)
+    await bot.send_message(
+      chat_id=CHAT_ID, 
+      text=text, 
+      message_thread_id=THREAD_ID
+    )
 
   asyncio.run_coroutine_threadsafe(send_error(trace), loop)
