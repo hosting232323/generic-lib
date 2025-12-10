953e1e5887391f2a0560c7386818b37963fcb23b
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-12-10 10:40:39 +0100

    fix di vanni

--- api/telegram.py
+++ api/telegram.py
@@ -1,7 +1,7 @@
 import os
+import json
 import asyncio
 import threading
-import json
 from telegram import Bot
 from flask import request
 
@@ -28,19 +28,19 @@ def start_loop(loop):
 
 threading.Thread(target=start_loop, args=(loop,), daemon=True).start()
 
+
 def escape_md(text: str) -> str:
-  escape_chars = r'_*\[\]()~`>#+-=|{}.!'
-  for ch in escape_chars:
-    text = text.replace(ch, f"\\{ch}")
+  for ch in r'_*\[\]()~`>#+-=|{}.!':
+    text = text.replace(ch, f'\\{ch}')
   return text
 
 
 async def send_error(text):
   await Bot(os.environ['TELEGRAM_TOKEN']).send_message(
-    chat_id=CHAT_ID, 
-    text=text, 
+    chat_id=CHAT_ID,
+    text=text,
     message_thread_id=TELEGRAM_TOPIC[os.environ.get('PROJECT_NAME', 'default')],
-    parse_mode="MarkdownV2"
+    parse_mode='MarkdownV2',
   )
 
 
@@ -48,34 +48,21 @@ def send_telegram_error(trace: str, include_request: bool = False):
   if int(os.environ.get('IS_DEV', 1)) == 1:
     return
 
-  message = f"*Errore:*\n```\n{trace}\n```"
+  message = f'*Errore:*\n```\n{trace}\n```'
   if include_request:
-    req_json = extract_request_data()
-    escaped_req = escape_md(req_json)
-
-    message += f"\n\n*Request Data:*\n```\n{escaped_req}\n```"
-
+    message += f'\n\n*Request Data:*\n```\n{escape_md(extract_request_data())}\n```'
   asyncio.run_coroutine_threadsafe(send_error(message), loop)
 
 
 def extract_request_data():
-  request_info = {
-    'path': request.path,
-    'method': request.method
-  }
-
+  request_info = {'path': request.path, 'method': request.method, 'headers': dict(request.headers)}
   args = request.args.to_dict()
   if args:
     request_info['args'] = args
-
   form = request.form.to_dict()
   if form:
     request_info['form'] = form
-
   json_data = request.get_json(silent=True)
   if json_data is not None:
     request_info['json'] = json_data
-
-  request_info['headers'] = dict(request.headers)
-
   return json.dumps(request_info, indent=2, ensure_ascii=False)
