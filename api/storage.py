3eadf31270ebb370a893e23687703b10cbcd6536
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-12-13 16:52:26 +0100

    PUSH

--- api/storage.py
+++ api/storage.py
@@ -10,6 +10,7 @@ from werkzeug.utils import secure_filename
 load_dotenv()
 
 s3 = None
+S3_BUCKET = 'fastsite-postgres-backup'
 
 if 'AWS_ACCESS_KEY_ID' in os.environ and 'AWS_SECRET_ACCESS_KEY' in os.environ:
   s3 = boto3.client(
@@ -83,9 +84,9 @@ def list_files_in_s3(bucket, folder=''):
   return files
 
 
-def get_all_filenames(storage_type, folder, bucket=None, local_folder=None):
+def get_all_filenames(storage_type, folder, local_folder=None):
   if storage_type == 's3':
-    return list_files_in_s3(bucket, folder)
+    return list_files_in_s3(S3_BUCKET, folder)
   elif storage_type == 'local':
     full_path = os.path.join(local_folder, folder)
     if not os.path.exists(full_path):
@@ -95,27 +96,26 @@ def get_all_filenames(storage_type, folder, bucket=None, local_folder=None):
 
 def upload_file(storage_type, file_path, folder):
   filename = secure_filename(os.path.basename(file_path))
-  s3_bucket = 'fastsite-postgres-backup'
   if storage_type == 's3':
     s3_key = f'{folder}/{filename}'    
     with open(file_path, 'rb') as f:
-      upload_file_to_s3(f, s3_bucket, s3_key)
+      upload_file_to_s3(f, S3_BUCKET, s3_key)
   elif storage_type == 'local':
     dest_path = os.path.join(folder, filename)
     os.makedirs(folder, exist_ok=True)
     os.rename(file_path, dest_path)
 
 
-def delete_file(storage_type, key, bucket=None, local_folder=None):
+def delete_file(storage_type, key, local_folder=None):
   if storage_type == 's3':
-    delete_file_from_s3(bucket, key)
+    delete_file_from_s3(S3_BUCKET, key)
   elif storage_type == 'local':
     path = os.path.join(local_folder, key)
     os.remove(path)
 
 
-def manage_backups(storage_type, folder, bucket=None, local_folder=None):
-  backups = get_all_filenames(storage_type, folder, bucket=bucket, local_folder=local_folder)
+def manage_backups(storage_type, folder, local_folder=None):
+  backups = get_all_filenames(storage_type, folder, local_folder=local_folder)
 
   backups.sort()
   backup_days = int(os.environ.get('POSTGRES_BACKUP_DAYS', 14))
@@ -123,4 +123,4 @@ def manage_backups(storage_type, folder, bucket=None, local_folder=None):
   if len(backups) > backup_days:
     files_to_delete = backups[: len(backups) - backup_days]
     for key in files_to_delete:
-      delete_file(storage_type, key, bucket=bucket, local_folder=local_folder)
+      delete_file(storage_type, key, local_folder=local_folder)
