7a117686e6b673eeca9ddf46fd5564e7c16f2c5b
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-05-06 12:22:56 +0200

    push

--- database_api/backup.py
+++ database_api/backup.py
@@ -12,29 +12,9 @@ POSTGRES_BACKUP_DAYS = int(os.environ.get('POSTGRES_BACKUP_DAYS', 14))
 
 def data_export(db_url: str):
   filename = f'{datetime.now().strftime("%y%m%d%H%M%S")}.dump'
-  process = subprocess.Popen(
-    [
-      'pg_dump',
-      f'--dbname={db_url}',
-      '--blobs',
-      '--clean',
-      '-Fc',
-      '--verbose',
-      '-f',
-      filename
-    ],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    text=True
+  subprocess.run(
+    ['pg_dump', f'--dbname={db_url}', '--blobs', '--clean', '-Fc', '--verbose', '-f', filename], check=True
   )
-
-  for line in process.stdout:
-    print(line.strip())
-
-  process.wait()
-
-  if process.returncode != 0:
-    raise Exception("pg_dump fallito")
   return filename
 
 
@@ -59,32 +39,23 @@ def data_import(db_url: str, filename: str):
 
 
 def db_backup(db_url: str, folder: str, storage_type, subfolder: str = None):
-  def _backup_job():
-    try:
-      filename = data_export(db_url)
+  filename = data_export(db_url)
 
-      with open(filename, 'rb') as content:
-        upload_file(content, filename, folder, storage_type, subfolder)
+  with open(filename, 'rb') as content:
+    upload_file(content, filename, folder, storage_type, subfolder)
 
-      delete_file(filename, '', 'local')
+  delete_file(filename, '', 'local')
 
-      backups = get_all_filenames(folder, storage_type, subfolder)
-      dump_files = [f for f in backups if f.lower().endswith('.dump')]
-      dump_files.sort()
+  backups = get_all_filenames(folder, storage_type, subfolder)
+  dump_files = [f for f in backups if f.lower().endswith('.dump')]
+  dump_files.sort()
 
-      if len(dump_files) > POSTGRES_BACKUP_DAYS:
-        files_to_delete = dump_files[: len(dump_files) - POSTGRES_BACKUP_DAYS]
+  if len(dump_files) > POSTGRES_BACKUP_DAYS:
+    files_to_delete = dump_files[: len(dump_files) - POSTGRES_BACKUP_DAYS]
 
-        for file_to_delete in files_to_delete:
-          filename = os.path.basename(file_to_delete)
-          subfolder_path = os.path.dirname(file_to_delete) or None
+    for file_to_delete in files_to_delete:
+      filename = os.path.basename(file_to_delete)
+      subfolder_path = os.path.dirname(file_to_delete) or None
 
-          delete_file(filename, folder, storage_type, subfolder_path)
-
-    except Exception as e:
-      print(f'[db_backup ERROR] {e}')  # noqa: T201
-
-  thread = threading.Thread(target=_backup_job, daemon=True)
-  thread.start()
-
+      delete_file(filename, folder, storage_type, subfolder_path)
   return {'status': 'ok', 'message': 'Backup avviato in background'}
