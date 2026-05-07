014c21398d9c4f53580798de84368b64851551be
Vanni <hosting23232323@gmail.com>
2026-05-07 14:46:26 +0000

    Zip folder

--- api/settings.py
+++ api/settings.py
@@ -11,10 +11,9 @@ IS_DEV = int(os.environ.get('IS_DEV', 1)) == 1
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 PROJECT_NAME = os.environ.get('PROJECT_NAME', 'default')
 
-SFTP_USER = os.environ.get('SFTP_USER')
-SFTP_HOST = os.environ.get('SFTP_HOST')
 SERVER_NAME = os.environ.get('SERVER_NAME')
 BACKUP_FOLDER = os.environ.get('BACKUP_FOLDER')
 RESTIC_PASSWORD = os.environ.get('RESTIC_PASSWORD')
+BACKUP_SSH_CONFIG = os.environ.get('BACKUP_SSH_CONFIG')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
--- api/storage/server.py
+++ api/storage/server.py
@@ -1,16 +1,16 @@
 import os
+import shutil
+import tempfile
 import subprocess
 
-from ..settings import SFTP_USER, SFTP_HOST, RESTIC_PASSWORD, BACKUP_FOLDER, SERVER_NAME
+from .utils import get_local_key
+from ..settings import BACKUP_SSH_CONFIG, RESTIC_PASSWORD, BACKUP_FOLDER, SERVER_NAME
 
 
 def storage_decorator(func):
   def wrapper(*args, **kwargs):
-    if not SFTP_USER:
-      raise ValueError('SFTP_USER non configurato')
-
-    if not SFTP_HOST:
-      raise ValueError('SFTP_HOST non configurato')
+    if not BACKUP_SSH_CONFIG:
+      raise ValueError('BACKUP_SSH_CONFIG non configurato')
 
     return func(*args, **kwargs)
 
@@ -19,13 +19,95 @@ def storage_decorator(func):
 
 
 @storage_decorator
-def folder_backup_server(folder_to_backup):
-  env = os.environ.copy()
+def upload_file_server(content, filename, folder, subfolder=None):
+  if subfolder:
+    key = f'{subfolder}/{filename}'
+  else:
+    key = get_local_key(filename)
 
+  remote_path = os.path.join(folder, key)
+
+  subprocess.run(
+    [
+      'ssh',
+      f'{BACKUP_SSH_CONFIG}',
+      f'mkdir -p "{os.path.dirname(remote_path)}"',
+    ],
+    check=True,
+  )
+
+  tmp_path = None
+  try:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+      shutil.copyfileobj(content, tmp)
+      tmp_path = tmp.name
+
+    subprocess.run(
+      [
+        'rsync',
+        '-avz',
+        '--partial',
+        '--append-verify',
+        tmp_path,
+        f'{BACKUP_SSH_CONFIG}:{remote_path}',
+      ],
+      check=True,
+    )
+
+  finally:
+    if tmp_path and os.path.exists(tmp_path):
+      os.remove(tmp_path)
+
+  return remote_path
+
+
+@storage_decorator
+def delete_file_server(filename, folder, subfolder=None):
+  if subfolder:
+    key = os.path.join(folder, subfolder)
+  elif folder:
+    key = os.path.join(folder, get_local_key(''))
+  else:
+    key = folder
+
+  subprocess.run(
+    [
+      'ssh',
+      f'{BACKUP_SSH_CONFIG}',
+      f'rm "{os.path.join(key, filename)}"',
+    ],
+    check=True,
+  )
+
+
+@storage_decorator
+def list_files_server(folder, subfolder=None):
+  if subfolder:
+    key = subfolder
+  else:
+    key = f'{folder}/{get_local_key("")}'
+
+  result = subprocess.run(
+    [
+      'ssh',
+      f'{BACKUP_SSH_CONFIG}',
+      f'find "{os.path.join(folder, key)}" -maxdepth 1 -type f -printf "%f\\n"',
+    ],
+    capture_output=True,
+    text=True,
+    check=True,
+  )
+
+  return [os.path.join(key, file) for file in result.stdout.strip().splitlines()]
+
+
+@storage_decorator
+def folder_backup_server(folder_to_backup):
   if not RESTIC_PASSWORD:
     raise ValueError('RESTIC_PASSWORD non configurata')
-  env['RESTIC_PASSWORD'] = RESTIC_PASSWORD
 
+  env = os.environ.copy()
+  env['RESTIC_PASSWORD'] = RESTIC_PASSWORD
   if not BACKUP_FOLDER:
     raise ValueError('BACKUP_FOLDER non configurata')
 
@@ -36,7 +118,7 @@ def folder_backup_server(folder_to_backup):
     [
       'restic',
       '-r',
-      f'sftp:{SFTP_USER}@{SFTP_HOST}:{os.path.join(BACKUP_FOLDER, "folder-backup")}',
+      f'sftp:{BACKUP_SSH_CONFIG}:{os.path.join(BACKUP_FOLDER, "folder-backup")}',
       'backup',
       folder_to_backup,
       '--host',
