c31949c33a9578ad5c1c87e1a64ad660cfd01294
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-06-12 11:02:01 +0200

    db import into docker

--- database_api/backup.py
+++ database_api/backup.py
@@ -5,15 +5,22 @@ import subprocess
 from datetime import datetime
 
 from api.telegram import send_telegram_message
-from api.settings import POSTGRES_BACKUP_DAYS, BACKUP_FOLDER
+from api.settings import POSTGRES_BACKUP_DAYS, BACKUP_FOLDER, POSTGRES_DOCKER_CONTAINER
 from api.storage import upload_file, get_all_filenames, delete_file
+
+
+PG_DUMP_FLAGS = ['--blobs', '--clean', '-Fc', '--verbose']
+PG_RESTORE_FLAGS = ['--verbose', '--clean', '--if-exists', '--no-privileges', '--no-owner']
 
 
 def data_export(db_url: str):
   filename = f'{datetime.now().strftime("%y%m%d%H%M%S")}.dump'
-  subprocess.run(
-    ['pg_dump', f'--dbname={db_url}', '--blobs', '--clean', '-Fc', '--verbose', '-f', filename], check=True
-  )
+
+  if POSTGRES_DOCKER_CONTAINER:
+    _docker_pg_dump(db_url, filename)
+  else:
+    _local_pg_dump(db_url, filename)
+
   return filename
 
 
@@ -22,19 +29,10 @@ def data_import(db_url: str, filename: str):
     print(f'File non trovato: {filename}')  # noqa: T201
     sys.exit(1)
 
-  subprocess.run(
-    [
-      'pg_restore',
-      f'--dbname={db_url}',
-      '--verbose',
-      '--clean',
-      '--if-exists',
-      '--no-privileges',
-      '--no-owner',
-      filename,
-    ],
-    check=True,
-  )
+  if POSTGRES_DOCKER_CONTAINER:
+    _docker_pg_restore(db_url, filename)
+  else:
+    _local_pg_restore(db_url, filename)
 
 
 def db_backup(db_url: str, storage_type):
@@ -72,3 +70,41 @@ def db_backup(db_url: str, storage_type):
 
   thread = threading.Thread(target=run, daemon=True)
   thread.start()
+
+
+def _docker_pg_dump(db_url: str, filename: str):
+  container_path = f'/tmp/{filename}'
+  _run_in_container(['pg_dump', f'--dbname={db_url}', *PG_DUMP_FLAGS, '-f', container_path])
+  try:
+    _copy_from_container(container_path, filename)
+  finally:
+    _run_in_container(['rm', container_path])
+
+
+def _docker_pg_restore(db_url: str, filename: str):
+  container_path = f'/tmp/{os.path.basename(filename)}'
+  _copy_to_container(filename, container_path)
+  try:
+    _run_in_container(['pg_restore', f'--dbname={db_url}', *PG_RESTORE_FLAGS, container_path])
+  finally:
+    _run_in_container(['rm', container_path])
+
+
+def _local_pg_dump(db_url: str, filename: str):
+  subprocess.run(['pg_dump', f'--dbname={db_url}', *PG_DUMP_FLAGS, '-f', filename], check=True)
+
+
+def _local_pg_restore(db_url: str, filename: str):
+  subprocess.run(['pg_restore', f'--dbname={db_url}', *PG_RESTORE_FLAGS, filename], check=True)
+
+
+def _run_in_container(command: list):
+  subprocess.run(['docker', 'exec', POSTGRES_DOCKER_CONTAINER, *command], check=True)
+
+
+def _copy_to_container(host_path: str, container_path: str):
+  subprocess.run(['docker', 'cp', host_path, f'{POSTGRES_DOCKER_CONTAINER}:{container_path}'], check=True)
+
+
+def _copy_from_container(container_path: str, host_path: str):
+  subprocess.run(['docker', 'cp', f'{POSTGRES_DOCKER_CONTAINER}:{container_path}', host_path], check=True)
