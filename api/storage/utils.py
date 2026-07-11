b9d63501e6368f0579c16b7a8744d2a1fe882d38
Vanni <hosting23232323@gmail.com>
2026-07-11 14:35:06 +0000

    Photos

--- api/storage/utils.py
+++ api/storage/utils.py
@@ -1,16 +1,10 @@
 import os
+from flask import request
+from sqlalchemy import text
+from sqlalchemy.orm import Session as session_type
 
-from ..settings import IS_DEV, RESTIC_PASSWORD, BACKUP_FOLDER, SERVER_NAME
-
-
-def get_full_path(folder, subfolder, ignore_dev, filename=None):
-  if not ignore_dev:
-    folder = os.path.join(folder, 'test' if IS_DEV else 'prod')
-
-  if subfolder:
-    folder = os.path.join(folder, subfolder)
-
-  return os.path.join(folder, filename) if filename else folder
+from database_api.operations import db_session_decorator
+from ..settings import RESTIC_PASSWORD, BACKUP_FOLDER, SERVER_NAME, API_PREFIX
 
 
 def format_mismatch_message(first_list: list, second_list: list, success_text: str, failure_text: str):
@@ -35,3 +29,27 @@ def set_backup_env():
   env = os.environ.copy()
   env['RESTIC_PASSWORD'] = RESTIC_PASSWORD
   return env
+
+
+@db_session_decorator(commit=True)
+def guess_next_id(model: str, session: session_type = None) -> int:
+  return session.execute(text(f"SELECT nextval('{model}_id_seq')")).scalar()
+
+
+def guess_extension(mime_type: str) -> str:
+  if mime_type == 'image/jpeg':
+    return '.jpg'
+  if mime_type == 'image/png':
+    return '.png'
+  if mime_type == 'image/webp':
+    return '.webp'
+  if mime_type == 'video/mp4':
+    return '.mp4'
+  if mime_type == 'application/pdf':
+    return '.pdf'
+
+  raise ValueError('Mime type non supportato')
+
+
+def get_base_file_path(path):
+  return f'{request.scheme}://{request.host}{f"/{API_PREFIX}" if API_PREFIX else ""}/{path}/'
